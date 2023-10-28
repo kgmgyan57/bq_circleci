3a5741c20efd504b1294cae3c5922a982d222047
mgyankumar <gyan.kumar@zalora.com>
2023-10-28 08:47:17 +0530

    test_45

---
+++ datamesh-ddl/table_2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS zalora-dwh.staging.mgktable2
+(
+    column_1 int64,
+    column_2 int64,
+    column_3 string,
+    column_4 string,
+    column_5 numeric,
+    column_6 string
+);
